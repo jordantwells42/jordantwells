0aa1a436deb9b38d24f506c640386d4ad4db6c15
Jordan Wells <jordantwells@gmail.com>
2020-08-03 01:31:18 -0500

    added django.contrib.sites

--- jordantwells/settings.py
+++ jordantwells/settings.py
@@ -43,9 +43,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
     'ckeditor',
     'disqus',
+    'django.contrib.sites',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
