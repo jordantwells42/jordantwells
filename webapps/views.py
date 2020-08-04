7af4103db5c3f5d79600e72696ddabbb7e6a29aa
Jordan Wells <jordantwells@gmail.com>
2020-08-03 19:40:15 -0500

    Added basic implentation of webapps

--- webapps/views.py
+++ webapps/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import WebApp
 
 # Create your views here.
 def webapps(request):
-	return render(request, 'webapps/webapps_index.html')
+	WebApps = WebApp.objects.all()
+	context = {
+		"WebApps" : WebApps
+	}
+	return render(request, 'webapps/webapps_index.html', context)
 
