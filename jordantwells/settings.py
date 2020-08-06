060a84d6120cded034154ca1ed47b7523fe37302
Jordan Wells <jordantwells@gmail.com>
2020-08-05 19:38:07 -0500

    security fixes

--- jordantwells/settings.py
+++ jordantwells/settings.py
@@ -214,3 +214,8 @@ CKEDITOR_CONFIGS = {
 }
 
 django_heroku.settings(locals())
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_REFERRER_POLICY = "same-origin"
