2f437462a566c2f4f4fad9c2a834e2aeb14eeb20
Jordan Wells <jordantwells@gmail.com>
2020-08-08 01:45:23 -0500

    small formatting changes

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,12 @@ from .models import Post
 # Create your views here.
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
+    
     context = {
         "posts": posts,
         "title": "Blog"
     }
+    
     return render(request, "blog/blog_index.html", context)
 
 def post_detail(request, pk):
